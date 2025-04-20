56f774c84e96287729ef61f14c06d92a72bfcc1d
Shivam-Jangid <shivamjangid1125@gmail.com>
2025-04-21 00:52:01 +0530

    new card created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  return <div style={{display:'flex'}}>
+    <Card>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>write your name here</span>
+        <input type="text" placeholder="name" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </Card>
+    <Card>
+      <div>
+        hi there
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </Card>
 
+
+    </div>
+}
+function Card({children}){
+  return <div style={{borderRadius:'20px', backgroundColor:'brown', padding:'20px', margin:'20px', color:'white',fontSize:'20px',display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}> 
+  {children}
+  </div>
+}
 export default App
