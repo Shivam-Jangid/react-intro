f75685da2e9ac80394e6275b122e3396157b16cd
Shivam-Jangid <shivamjangid1125@gmail.com>
2025-04-23 13:52:54 +0530

    navigation hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
-
+import { BrowserRouter, Link, Route, Routes, useNavigate } from "react-router-dom"
 
 function App() {
-  return <div style={{display:'flex'}}>
-    <Card>
-      <div>
-        <span>write your name here</span>
-        <input type="text" placeholder="name" />
-      </div>
-    </Card>
-    <Card>
-      <div>
-        hi there
-      </div>
-    </Card>
+  return <div>
 
 
-    </div>
+    <BrowserRouter>
+    <Link to='/class-12'> Class 12</Link>
+    <Link to='/class-11'>Class 11</Link>
+    <Routes>
+    <Route path="/" element ={<div>Landing page</div>}  ></Route>
+
+
+      <Route path="/class-11" element =  {<div>class 11</div>}></Route>
+      <Route path="/class-12" element = {<Class12Program/>} ></Route>
+      <Route path="*" element={<ErrorPage/>}></Route>
+
+
+    </Routes>
+    </BrowserRouter>
+
+  
+  </div>
 }
-function Card({children}){
-  return <div style={{borderRadius:'20px', backgroundColor:'brown', padding:'20px', margin:'20px', color:'white',fontSize:'20px',display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}> 
-  {children}
+
+function Class12Program(){
+  const nav = useNavigate();
+  function navigateToClass11(){
+    nav('/class-11')
+  }
+  return <div >
+    hi there
+    <button onClick={navigateToClass11}>Click to go to class 11</button>
+  </div>
+}
+function ErrorPage(){
+  const nav = useNavigate();
+  function homeNav(){
+    nav('/');
+  }
+  return <div>
+    page not found , Go to 
+    <button onClick={homeNav}>home</button>
   </div>
 }
 export default App
